5b1946e1d991f63aa09a23df8f4785a789a2fbe0
Projetos Pessoais <gabriel20silver@hotmail.com>
2024-09-14 14:00:19 -0300

    Ajustada estrutura de pastas de app

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,28 @@
+import Loading from "@/src/components/Loading";
+import { useAuthContext } from "../../contexts/AuthContext";
+import { Redirect, Tabs } from "expo-router";
+import { StatusBar, Text, View } from "react-native";
+
+export default function AppLayout() {
+  const { isLoading, isAuthenticated, user } = useAuthContext();
+
+    if (isLoading) {
+        return <Loading/>
+    }
+
+    if (!isAuthenticated) {
+        return <Redirect href={"/login"}/> 
+    }
+
+
+  return (
+      <Tabs>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Produtos"
+          }}
+        />
+      </Tabs>
+  );
+}
