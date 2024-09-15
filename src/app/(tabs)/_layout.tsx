59bda83b2d276ed8d8c9bb9a795ae625855c9fd6
Projetos Pessoais <gabriel20silver@hotmail.com>
2024-09-15 18:59:42 -0300

    Implementada função de cadastro e login completa, controle de autenticação implementado, tambem realizada padronização dos nomes em ingles em todas as paginas

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -6,23 +6,23 @@ import { StatusBar, Text, View } from "react-native";
 export default function AppLayout() {
   const { isLoading, isAuthenticated, user } = useAuthContext();
 
-    if (isLoading) {
-        return <Loading/>
-    }
+  if (isLoading) {
+    return <Loading />
+  }
 
-    if (!isAuthenticated) {
-        return <Redirect href={"/login"}/> 
-    }
+  if (!isAuthenticated) {
+    return <Redirect href={"/login"} />
+  }
 
 
   return (
-      <Tabs>
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: "Produtos"
-          }}
-        />
-      </Tabs>
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Produtos"
+        }}
+      />
+    </Tabs>
   );
 }
