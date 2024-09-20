74f4129a333a4a9a93ffcd530ed751cb5cd32a73
Projetos Pessoais <gabriel20silver@hotmail.com>
2024-09-19 23:48:50 -0300

    Implementado sincprovider, realizado teste de getProduct offline e online

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,19 +1,26 @@
-import { Slot} from "expo-router";
+import { Slot } from "expo-router";
 import { AuthProvider } from "../contexts/AuthContext";
+import { initializeDatabase } from "@/src/database/initializeDatabase";
 import { StatusBar } from "expo-status-bar";
 import { ImageBackground, View } from "react-native";
 import { IMAGE_PATHS } from "../../styles/constants";
 import { globalStyles } from "@/styles/styles";
+import { SQLiteProvider } from "expo-sqlite";
+import { SyncProvider } from "../contexts/SyncContext";
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <View style={{ flex: 1 }}>
-        <StatusBar style="light" />
-        <ImageBackground source={IMAGE_PATHS.backgroundImage} style={globalStyles.backgroundImage}>
-          <Slot />
-        </ImageBackground>
-      </View>
-    </AuthProvider>
+    <SQLiteProvider databaseName="myDataBase.db" onInit={initializeDatabase}>
+      <SyncProvider>
+        <AuthProvider>
+          <View style={{ flex: 1 }}>
+            <StatusBar style="light" />
+            <ImageBackground source={IMAGE_PATHS.backgroundImage} style={globalStyles.backgroundImage}>
+              <Slot />
+            </ImageBackground>
+          </View>
+        </AuthProvider>
+      </SyncProvider>
+    </SQLiteProvider>
   );
 }
