7a057d2b861be7f1b064786c04f10287352913f3
Projetos Pessoais <gabriel20silver@hotmail.com>
2024-10-04 23:28:11 -0300

    Criada componente de valor hora

--- src/app/(tabs)/log.tsx
+++ src/app/(tabs)/log.tsx
@@ -1,43 +1,58 @@
+import Button from '@/src/components/Button';
+import { useAuthContext } from '@/src/contexts/AuthContext';
 import usePendingPaymentDatabase from '@/src/database/usePendingPaymentDatabase';
 import usePeopleDatabase from '@/src/database/usePeopleDatabase';
 import { useIsFocused } from '@react-navigation/native';
+import { useSQLiteContext } from 'expo-sqlite';
 import { useEffect, useState } from 'react';
 import { View, Text, FlatList } from 'react-native'
 
 export default function log() {
-    const pendingPaymentDatabase = usePendingPaymentDatabase();
-    const usePeople = usePeopleDatabase()
-    const [listPendingPayment, setListPendingPayment ] = useState([])
+  const pendingPaymentDatabase = usePendingPaymentDatabase();
+  const usePeople = usePeopleDatabase()
+  const [listPendingPayment, setListPendingPayment] = useState([])
 
-    async function getTablePendingPayment() {
-        const response = await usePeople.getPeople()
-        
-        setListPendingPayment(response)
+  const { signIn, signOut, isAuthenticated } = useAuthContext()
+  const database = useSQLiteContext()
+
+  async function logout() {
+    const tables = await database.getAllAsync(`SELECT name FROM sqlite_master WHERE type='table'`);
+    for (const table of tables) {
+      await database.execAsync(`DELETE FROM ${table.name}`);
     }
+    signOut()
+  }
+  async function getTablePendingPayment() {
+    const response = await usePeople.getPeople()
+
+    setListPendingPayment(response)
+  }
 
 
-    const isFocused = useIsFocused();
-    useEffect(() => {
-        getTablePendingPayment()
-    },[isFocused])
+  const isFocused = useIsFocused();
+  useEffect(() => {
+    getTablePendingPayment()
+  }, [isFocused])
 
-    return (
-        <View>
-            <FlatList
-            data={ listPendingPayment}
-            keyExtractor={item => item.id_pessoa}
-            renderItem={
-              ({ item }) => (
-                <View style={{marginTop: 30}}>
-                  <Text>id_pagamento {item.email}</Text>
-                  <Text>id_pessoa {item.id_pessoa}</Text>
-                  <Text>nome {item.nome}</Text>
-                  <Text>ultimo_pagamento {item.id_pessoa}</Text>
-                  <Text>total {item.id_perfil}</Text>
-                </View>
-              )
-            }
-          />
-        </View>
-    )
+  return (
+    <View>
+      <Text>Vc esta esta logado</Text>
+      <Button onPress={logout} title={"Sign Out"}></Button>
+      <FlatList
+        data={listPendingPayment}
+        keyExtractor={item => item.id_pessoa}
+        renderItem={
+          ({ item }) => (
+            <View style={{ marginTop: 30 }}>
+              <Text>id_pagamento {item.email}</Text>
+              <Text>id_pessoa {item.id_pessoa}</Text>
+              <Text>nome {item.nome}</Text>
+              <Text>ultimo_pagamento {item.id_pessoa}</Text>
+              <Text>total {item.id_perfil}</Text>
+            </View>
+          )
+        }
+      />
+    </View>
+  )
 }
--- src/app/(tabs)/produtos.tsx
+++ src/app/(tabs)/produtos.tsx
@@ -1,24 +1,66 @@
 import { useAuthContext } from "@/src/contexts/AuthContext";
+import { colors } from "@/styles/color";
+import { IMAGE_PATHS } from "@/styles/constants";
+import { globalStyles } from "@/styles/styles";
+import { Ionicons } from "@expo/vector-icons";
+import { useIsFocused } from "@react-navigation/native";
 import { router } from "expo-router";
 import { useSQLiteContext } from "expo-sqlite";
-import { Button, Text, View } from "react-native";
+import { useEffect } from "react";
+import { Button, ImageBackground, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Produtos() {
-  const { signIn, signOut, isAuthenticated } = useAuthContext()
-  const database = useSQLiteContext()
 
-  async function logout() {
-    const tables = await database.getAllAsync(`SELECT name FROM sqlite_master WHERE type='table'`);
-    for (const table of tables) {
-      await database.execAsync(`DELETE FROM ${table.name}`);
-    }    
-    signOut()
-  }
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+
+  }, [isFocused]);
+
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Vc esta esta logado</Text>
-      <Button onPress={logout} title={"Sign Out"}></Button>
-    </View>
+    <ImageBackground source={IMAGE_PATHS.backgroundImage} style={globalStyles.backgroundImage}>
+      <SafeAreaView style={globalStyles.pageContainer}>
+        <View style={[globalStyles.container, styles.productContainer]}>
+          <View style={styles.titleContainer}>
+            <Ionicons name={"arrow-back-outline"} size={35} color={colors.primary} />
+            <Text style={globalStyles.title}>Produtos</Text>
+          </View>
+          <View style={styles.hourContainer}>
+            <Text style={styles.hourText}>Valor Hora: R$20,00</Text>
+            <Ionicons name={"create-outline"} size={30} color={colors.primary} />
+          </View>
+        </View>
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  productContainer: {
+    padding: 8,
+    flex: 1,
+    flexGrow: 1
+  },
+  titleContainer:{
+    padding: 8,
+    gap:8,
+    flexDirection:"row",
+    alignItems: "center"
+  },
+  hourContainer: {
+    flexDirection: "row",
+    width: "100%",
+    borderRadius: 4,
+    padding: 12,
+    backgroundColor: colors.backgroundTertiary,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 8
+  },
+  hourText: {
+    fontSize: 16,
+    color: colors.text
+  }
+})
